7172464fe87c74b17cc0489293b5341b753bc4a4
Arlen Vasconcelos <arlen.vasconcelos@gmail.com>
2020-04-25 22:58:02 -0300

    modify user migration

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -7,8 +7,11 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
+      table.string('name', 254).notNullable()
+      table.string('identification_number', 254).notNullable().unique()
+      table.string('type', 254).notNullable()
       table.string('email', 254).notNullable().unique()
+      table.string('phone', 254)
       table.string('password', 60).notNullable()
       table.timestamps()
     })
